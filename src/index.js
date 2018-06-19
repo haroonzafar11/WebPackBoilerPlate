8a0f07a673b496d080b5aafea130ac0ebd877515
Haroon Zafar <harronzafar@gmail.com>
2018-06-20 01:19:47 +0500

    initial commit with gitignore

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from "react-dom";
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import {createLogger} from 'redux-logger';
+import allReducers from './reducers';
+import App from './components/App';
+
+const logger = createLogger();
+const store = createStore(
+    allReducers,
+    applyMiddleware(logger)
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('index')
+);
